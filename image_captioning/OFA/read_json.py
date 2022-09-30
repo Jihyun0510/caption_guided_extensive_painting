59c43bbe4d6f757f5908d50b7d2f1a06087781ec
Jihyun Kim <78332579+Jihyun0510@users.noreply.github.com>
2022-09-30 14:02:42 +0900

    Update read_json.py

--- image_captioning/OFA/read_json.py
+++ image_captioning/OFA/read_json.py
@@ -1,10 +1,10 @@
 import json
 
-path = "/home/wgus5950/OFA/results/caption/"
+path = ""
 with open (path + "test_predict.json", "r") as f:
     data = json.load(f)
 
-save_path ="/database/jhkim/final_random_checkpoints/landmark/4000_texts/"
+save_path =""
 
 for d in data:
     f = open(save_path+d['image_id']+".txt","w")
